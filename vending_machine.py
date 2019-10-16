cbdb11d4ff5f0d83414200c40c88d4df6929810e
Atsushi Mori <ki4070ma@gmail.com>
2019-10-16 23:34:19 +0900

    Add vending machine class

---
+++ vending_machine.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class VendingMachine(object):
+
+    def foo(self):
+        print('bar')
+
+
+if __name__ == '__main__':
+
+    vending_machine = VendingMachine()
+    vending_machine.foo()
