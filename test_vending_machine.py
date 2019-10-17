c8187d1a5b208e3762ff36506718a363373f904c
Atsushi Mori <ki4070ma@gmail.com>
2019-10-18 03:34:37 +0900

    Changed code style
    test: unittest -> pytest
    code: return -> print

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -1,18 +1,28 @@
+import sys
 from unittest import TestCase
 from vending_machine import VendingMachine
+from io import StringIO
+import pytest
 
-class TestVendingMachine(TestCase):
+class TestVendingMachine(object):
 
-    def test_insert_available_money(self):
-        vm = VendingMachine()
-        assert vm.insert(10) == 0
-        assert vm.insert(50) == 0
-        assert vm.insert(100) == 0
-        assert vm.insert(500) == 0
-        assert vm.insert(1000) == 0
+    def setup_method(self, method):
+        print('method{}'.format(method.__name__))
+        self.vm = VendingMachine()
+        self.captor = StringIO()
+        sys.stdout = self.captor
 
-    def test_insert_unavailable_money(self):
-        vm = VendingMachine()
-        assert vm.insert(-1) == -1
-        assert vm.insert(3) == 3
-        assert vm.insert(11) == 11
+    def teardown_method(self, method):
+        sys.stdout = sys.__stdout__
+        print('method{}:'.format(method.__name__))
+        del self.vm
+
+    @pytest.mark.parametrize('money', [10, 50, 100, 500, 1000])
+    def test_insert_available_money(self, capsys, money):
+        self.vm.insert(money)
+        assert capsys.readouterr().out == 'changes: 0\n'
+
+    @pytest.mark.parametrize('money', [-1, 3, 11])
+    def test_insert_unavailable_money(self, capsys, money):
+        self.vm.insert(money)
+        assert capsys.readouterr().out == 'changes: {}\n'.format(money)
--- vending_machine.py
+++ vending_machine.py
@@ -6,10 +6,15 @@ class VendingMachine(object):
         self.inserted_money = 0
 
     def insert(self, money):
+        changes = money
         if money in [10, 50, 100, 500, 1000]:
             self.inserted_money += money
-            return 0
-        return money
+            changes = 0
+        print('changes: {}'.format(changes))
+
+    def refund(self):
+        print('changes: {}'.format(self.inserted_money))
+        self.inserted_money = 0
 
 
 if __name__ == '__main__':
